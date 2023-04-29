546647a240d766fb386ae46b81a4eecc7fadc787
esemyashkina <119455181+esemyashkina@users.noreply.github.com>
2023-04-29 13:45:25 +0300

    Add files

---
+++ task_4.py
@@ -0,0 +1,5 @@
+num = int(input())
+if num % 2 == 0:
+    print('Число четное')
+else:
+    print('Число нечетное')
---
+++ task_7.py
@@ -0,0 +1,6 @@
+from task_6 import add
+
+
+a = int(input())
+b = int(input())
+print(add(a, b))
